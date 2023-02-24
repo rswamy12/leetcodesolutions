36e71f1a2d8f1f49a5377bbf47bb3d05f4c09352
rswamy12 <113652963+rswamy12@users.noreply.github.com>
2023-02-23 19:21:48 -0600

    Add files via upload

---
+++ leetcode13.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dict = {"I":1,
+                "V":5,
+                "X":10,
+                "L":50,
+                "C":100,
+                "D":500,
+                "M":1000}
+        s = s.replace("IV", "IIII").replace("IX","VIIII").replace("XL","XXXX").replace("XC","LXXXX").replace("CD", "CCCC").replace("CM","DCCCC")
+        sum = 0
+        for char in s:
+            sum += dict[char]
+        return sum 
